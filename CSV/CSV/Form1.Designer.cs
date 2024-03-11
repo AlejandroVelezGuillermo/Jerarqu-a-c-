499ba55e11f21ca35f1e42b9c51c7fd0b135adc6
AlejandroVelezGuillermo <125221168+AlejandroVelezGuillermo@users.noreply.github.com>
2024-03-11 07:56:03 -0600

    Add files via upload

---
+++ CSV/CSV/Form1.Designer.cs
@@ -0,0 +1,339 @@
+﻿namespace CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea7 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend7 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series7 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aGREGARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.gUARDARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aBRIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.eDITARToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.dgvDatos = new System.Windows.Forms.DataGridView();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.txtTelefono = new System.Windows.Forms.TextBox();
+            this.txtCorreo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aGREGARToolStripMenuItem,
+            this.gUARDARToolStripMenuItem,
+            this.aBRIRToolStripMenuItem,
+            this.eDITARToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(1579, 28);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aGREGARToolStripMenuItem
+            // 
+            this.aGREGARToolStripMenuItem.Name = "aGREGARToolStripMenuItem";
+            this.aGREGARToolStripMenuItem.Size = new System.Drawing.Size(89, 24);
+            this.aGREGARToolStripMenuItem.Text = "AGREGAR";
+            this.aGREGARToolStripMenuItem.Click += new System.EventHandler(this.aGREGARToolStripMenuItem_Click);
+            // 
+            // gUARDARToolStripMenuItem
+            // 
+            this.gUARDARToolStripMenuItem.Name = "gUARDARToolStripMenuItem";
+            this.gUARDARToolStripMenuItem.Size = new System.Drawing.Size(92, 24);
+            this.gUARDARToolStripMenuItem.Text = "GUARDAR";
+            this.gUARDARToolStripMenuItem.Click += new System.EventHandler(this.gUARDARToolStripMenuItem_Click);
+            // 
+            // aBRIRToolStripMenuItem
+            // 
+            this.aBRIRToolStripMenuItem.Name = "aBRIRToolStripMenuItem";
+            this.aBRIRToolStripMenuItem.Size = new System.Drawing.Size(64, 24);
+            this.aBRIRToolStripMenuItem.Text = "ABRIR";
+            this.aBRIRToolStripMenuItem.Click += new System.EventHandler(this.aBRIRToolStripMenuItem_Click);
+            // 
+            // eDITARToolStripMenuItem
+            // 
+            this.eDITARToolStripMenuItem.Name = "eDITARToolStripMenuItem";
+            this.eDITARToolStripMenuItem.Size = new System.Drawing.Size(72, 24);
+            this.eDITARToolStripMenuItem.Text = "EDITAR";
+            this.eDITARToolStripMenuItem.Click += new System.EventHandler(this.eDITARToolStripMenuItem_Click);
+            // 
+            // dgvDatos
+            // 
+            this.dgvDatos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatos.Location = new System.Drawing.Point(13, 164);
+            this.dgvDatos.Name = "dgvDatos";
+            this.dgvDatos.RowHeadersWidth = 51;
+            this.dgvDatos.RowTemplate.Height = 24;
+            this.dgvDatos.Size = new System.Drawing.Size(775, 294);
+            this.dgvDatos.TabIndex = 1;
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Location = new System.Drawing.Point(77, 38);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(272, 22);
+            this.txtNombre.TabIndex = 2;
+            // 
+            // txtTelefono
+            // 
+            this.txtTelefono.Location = new System.Drawing.Point(77, 66);
+            this.txtTelefono.Name = "txtTelefono";
+            this.txtTelefono.Size = new System.Drawing.Size(272, 22);
+            this.txtTelefono.TabIndex = 3;
+            // 
+            // txtCorreo
+            // 
+            this.txtCorreo.Location = new System.Drawing.Point(77, 94);
+            this.txtCorreo.Name = "txtCorreo";
+            this.txtCorreo.Size = new System.Drawing.Size(272, 22);
+            this.txtCorreo.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Nombre:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(64, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Telefono:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 100);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 16);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Correo:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(631, 31);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(133, 22);
+            this.textBox1.TabIndex = 8;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(489, 34);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(131, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Nombre Del Archivo:";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "csv",
+            "txt",
+            "xml",
+            "json"});
+            this.comboBox1.Location = new System.Drawing.Point(631, 59);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(56, 24);
+            this.comboBox1.TabIndex = 10;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(510, 67);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(110, 16);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Ruta Del Archivo:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(10, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(72, 16);
+            this.label6.TabIndex = 14;
+            this.label6.Text = "Asistencia:";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Asistio",
+            "No Asistio"});
+            this.comboBox2.Location = new System.Drawing.Point(88, 126);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(102, 24);
+            this.comboBox2.TabIndex = 15;
+            // 
+            // chart1
+            // 
+            chartArea7.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea7);
+            legend7.Name = "Legend1";
+            this.chart1.Legends.Add(legend7);
+            this.chart1.Location = new System.Drawing.Point(812, 22);
+            this.chart1.Name = "chart1";
+            series7.ChartArea = "ChartArea1";
+            series7.Legend = "Legend1";
+            series7.Name = "Series1";
+            this.chart1.Series.Add(series7);
+            this.chart1.Size = new System.Drawing.Size(755, 416);
+            this.chart1.TabIndex = 16;
+            this.chart1.Text = "chart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(355, 38);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Random";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(355, 66);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Random";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(355, 94);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 19;
+            this.button3.Text = "Random";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(812, 444);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(767, 23);
+            this.button4.TabIndex = 20;
+            this.button4.Text = "ACTUALIZAR";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1579, 470);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtCorreo);
+            this.Controls.Add(this.txtTelefono);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.dgvDatos);
+            this.Controls.Add(this.menuStrip1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aGREGARToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem gUARDARToolStripMenuItem;
+        private System.Windows.Forms.DataGridView dgvDatos;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.TextBox txtTelefono;
+        private System.Windows.Forms.TextBox txtCorreo;
+        private System.Windows.Forms.ToolStripMenuItem aBRIRToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ToolStripMenuItem eDITARToolStripMenuItem;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ CSV/CSV/Form1.cs
@@ -0,0 +1,297 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace CSV
+{
+    public partial class Form1 : Form
+    {
+        // Lista para almacenar los datos
+        private List<registros> registros = new List<registros>();
+        // Ruta del archivo CSV actualmente abierto
+        private string rutaArchivoActual = "";
+
+        string formato;
+        public Form1()
+        {
+            InitializeComponent();
+            // Establecer la propiedad DropDownStyle del ComboBox
+            comboBox1.DropDownStyle = ComboBoxStyle.DropDownList;
+        }
+        private void aGREGARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            AGREGAR();
+        }
+        private void AGREGAR() 
+        {
+            // Validar que los campos no estén vacíos
+            if (string.IsNullOrWhiteSpace(txtNombre.Text) || string.IsNullOrWhiteSpace(txtTelefono.Text) || string.IsNullOrWhiteSpace(txtCorreo.Text) || string.IsNullOrWhiteSpace(comboBox2.Text))
+            {
+                MessageBox.Show("Por favor, complete todos los campos antes de agregar.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            // Crear un nuevo registro
+            registros nuevoRegistro = new registros
+            {
+                Nombre = txtNombre.Text,
+                Telefono = txtTelefono.Text,
+                Correo = txtCorreo.Text,
+                Asistencia = comboBox2.Text,
+            };
+
+            // Agregar el registro a la lista y al DataGridView
+            registros.Add(nuevoRegistro);
+            dgvDatos.DataSource = null; // Limpiar el origen de datos actual
+            dgvDatos.DataSource = registros; // Asignar la nueva lista de registros
+            LimpiarCampos();
+        }
+        private void gUARDARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SAVE();
+        }
+        private void SAVE() 
+        {
+            string NombreA = textBox1.Text;
+            try
+            {
+                // Obtener la ruta del escritorio del usuario actual
+                string escritorio = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+
+                // Crear la ruta completa del archivo CSV en el escritorio
+                string rutaArchivo = Path.Combine(escritorio, NombreA + "." + formato);
+
+                // Crear y escribir en el archivo CSV
+                using (StreamWriter writer = new StreamWriter(rutaArchivo))
+                {
+                    // Escribir encabezados
+                    writer.WriteLine("Nombre,Telefono,Correo,Asistencia");
+
+                    // Escribir datos
+                    foreach (registros registro in registros)
+                    {
+                        writer.WriteLine($"{registro.Nombre},{registro.Telefono},{registro.Correo}");
+                    }
+                }
+
+                MessageBox.Show($"Datos guardados exitosamente en el archivo CSV en el escritorio ({rutaArchivo}).", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                LimpiarCampos();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Error al guardar en el archivo CSV: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            // Limpiar el DataGridView
+            dgvDatos.DataSource = null;
+            dgvDatos.Rows.Clear(); // Esto debería funcionar, pero si hay problemas, asegúrate de que el DataGridView está configurado correctamente
+
+            // Limpiar la lista de registros
+            registros.Clear();
+        }
+        private void LimpiarCampos()
+        {
+            LIMPIAR();
+        }
+        private void LIMPIAR()
+        {
+            // Limpiar los campos de entrada
+            txtNombre.Text = "";
+            txtTelefono.Text = "";
+            txtCorreo.Text = "";
+            textBox1.Text = "";
+            comboBox2.Text = "";
+        }
+        private void aBRIRToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Diálogo para abrir el archivo CSV
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Archivos CSV |*.csv |Archivos Txt|*.txt|Archivos xml|*.xml|Archivos json|*.json|Todos Los Archivos|*.*",
+                Title = "Archivos Cargados Correctamente."
+            };
+
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                // Obtener la ruta del archivo seleccionado
+                string rutaArchivo = openFileDialog.FileName;
+
+                // Leer datos desde el archivo CSV
+                using (StreamReader reader = new StreamReader(rutaArchivo))
+                {
+                    // Saltar la primera línea (encabezados)
+                    reader.ReadLine();
+
+                    // Limpiar la lista actual de registros
+                    registros.Clear();
+
+                    // Leer y agregar registros desde el archivo
+                    while (!reader.EndOfStream)
+                    {
+                        string[] campos = reader.ReadLine().Split(',');
+                        registros nuevoRegistro = new registros
+                        {
+                            Nombre = campos[0],
+                            Telefono = campos[1],
+                            Correo = campos[2]
+                        };
+                        registros.Add(nuevoRegistro);
+                    }
+                }
+
+                // Mostrar los datos en el DataGridView
+                dgvDatos.DataSource = null;
+                dgvDatos.DataSource = registros;
+
+                MessageBox.Show("Datos cargados exitosamente.", "Éxito", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private void rEMPLACARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Obtener el valor seleccionado del ComboBox
+            object valorSeleccionado = comboBox1.SelectedItem;
+
+            // Si es una cadena, puedes convertirlo a string si es necesario
+            if (valorSeleccionado != null)
+            {
+               formato = valorSeleccionado.ToString();
+            }
+        }
+        private void eDITARToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            // Verificar si hay al menos un renglón seleccionado
+            if (dgvDatos.SelectedRows.Count > 0)
+            {
+                // Obtener el índice del primer renglón seleccionado
+                int indiceSeleccionado = dgvDatos.SelectedRows[0].Index;
+
+                // Obtener la fila completa del renglón seleccionado
+                DataGridViewRow filaSeleccionada = dgvDatos.Rows[indiceSeleccionado];
+
+                // Obtener los valores de todas las celdas del renglón
+                string valorCelda0 = filaSeleccionada.Cells[0].Value.ToString();
+                string valorCelda1 = filaSeleccionada.Cells[1].Value.ToString();
+                string valorCelda2 = filaSeleccionada.Cells[2].Value.ToString();
+
+                // Mostrar los valores en los TextBox
+                txtNombre.Text = valorCelda0;
+                txtTelefono.Text = valorCelda1;
+                txtCorreo.Text = valorCelda2;
+            }
+        }
+        private void ActualizarGrafico()
+        {
+            // Limpiar la serie del gráfico antes de actualizar
+            chart1.Series.Clear();
+
+            // Crear una nueva serie para la columna de asistencia
+            Series seriesAsistencia = new Series("Asistencia");
+
+            // Contar la cantidad de asistencias para cada opción
+            var conteoAsistencias = registros
+                .GroupBy(r => r.Asistencia)
+                .Select(g => new { Asistencia = g.Key, Cantidad = g.Count() });
+
+            // Agregar los puntos al gráfico
+            foreach (var item in conteoAsistencias)
+            {
+                seriesAsistencia.Points.AddXY(item.Asistencia, item.Cantidad);
+            }
+
+            // Agregar la serie al gráfico
+            chart1.Series.Add(seriesAsistencia);
+
+            // Configurar el gráfico según tus preferencias
+            chart1.ChartAreas[0].AxisX.Title = "Asistencia";
+            chart1.ChartAreas[0].AxisY.Title = "Cantidad";
+            chart1.Series["Asistencia"].ChartType = SeriesChartType.Column;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // Array de nombres aleatorios
+            string[] nombres = { "Juan", "María", "Pedro", "Ana", "Carlos", "Laura", "José", "Isabel", "Miguel", "Elena" };
+
+            // Crear una instancia de la clase Random para generar números aleatorios
+            Random random = new Random();
+
+            // Obtener un índice aleatorio del array de nombres
+            int indiceAleatorio = random.Next(nombres.Length);
+
+            // Mostrar el nombre aleatorio en el TextBox
+            txtNombre.Text = nombres[indiceAleatorio];
+        }
+
+        private string GenerarNumeroTelefono()
+        {
+            Random random = new Random();
+
+            // Generar cada parte del número de teléfono
+            string parte1 = random.Next(100, 1000).ToString("000");
+            string parte2 = random.Next(100, 1000).ToString("000");
+            string parte3 = random.Next(1000, 10000).ToString("0000");
+
+            // Formar el número completo
+            string numeroCompleto = $"({parte1}) {parte2}-{parte3}";
+
+            return numeroCompleto;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Generar un número de teléfono aleatorio
+            string numeroAleatorio = GenerarNumeroTelefono();
+
+            // Mostrar el número aleatorio en el TextBox
+            txtTelefono.Text = numeroAleatorio;
+        }
+
+        private string GenerarCorreoElectronico()
+        {
+            Random random = new Random();
+
+            // Dominios de correo electrónico posibles
+            string[] dominios = { "gmail.com", "yahoo.com", "outlook.com", "example.com", "domain.com" };
+
+            // Generar una parte inicial del correo aleatoria
+            string parteInicial = Guid.NewGuid().ToString().Substring(0, 8);
+
+            // Seleccionar aleatoriamente un dominio
+            string dominio = dominios[random.Next(dominios.Length)];
+
+            // Formar la dirección de correo electrónico completa
+            string correoCompleto = $"{parteInicial}@{dominio}";
+
+            return correoCompleto;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // Generar una dirección de correo electrónico aleatoria
+            string correoAleatorio = GenerarCorreoElectronico();
+
+            // Mostrar la dirección aleatoria en el TextBox
+            txtCorreo.Text = correoAleatorio;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            ActualizarGrafico();
+        }
+    }
+}
